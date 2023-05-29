ee7bd47985e2922a533c1cc29f18cd014be35344
Thiago Sotoriva Lermen <thiago.lermen@gmail.com>
2023-05-29 13:56:40 +0200

    Bug fixes and implemented PDF generation for My Tickets page

--- WebContent/scripts/list_tickets.js
+++ WebContent/scripts/list_tickets.js
@@ -7,6 +7,71 @@ function initView(scope) {
     scope.alertCreateAccount = false;
 }
 
+function printPdf(ticket) {
+	
+	var props = {
+			outputType: jsPDFInvoiceTemplate.OutputType.Save,
+    	    returnJsPDFDocObject: true,
+    	    fileName: "ticket",
+    	    orientationLandscape: false,
+    	    compress: true,
+    	    logo: {
+    	        src: "../assets/logo-blue-my-travel.png",
+    	        type: 'PNG', //optional, when src= data:uri (nodejs case)
+    	        width: 70, //aspect ratio = width/height
+    	        height: 25,
+    	        margin: {
+    	            top: 0, //negative or positive num, from the current position
+    	            left: 0 //negative or positive num, from the current position
+    	        }
+    	    },
+    	    stamp: {
+                inAllPages: true,
+                src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
+                width: 20, //aspect ratio = width/height
+                height: 20,
+                margin: {
+                    top: 100, //negative or positive num, from the current position
+                    left: 0 //negative or positive num, from the current position
+                }
+            },
+    	    business: {},
+    	    contact: {
+    	        label: "Ticket issued for:",
+    	        name: ticket.firstName + ticket.lastName,
+    	        address: "Passport: " + ticket.passportNumber,
+    	        phone: "Birth date: " + ticket.birthDateString,
+    	    },
+    	    invoice: {
+    	        label: "Ticket # " ,
+    	        num: ticket.ticketId,
+    	        headerBorder: false,
+    	        tableBodyBorder: false,
+    	        header: [
+    	          { title: "Departure date",}, 
+    	          { title: "Departure airport",}, 
+    	          { title: "Arrival airport"}, 
+    	          { title: "Seat"},
+    	          { title: "Aircraft"},
+    	          { title: "Price"}
+    	        ],
+    	        table: Array.from(Array(1), (item, index)=>([
+    	            ticket.flight.departureDate,
+    	            ticket.flight.departureAirport.airportName,
+    	            ticket.flight.arrivalAirport.airportName,
+    	            ticket.seatNumber,
+    	            ticket.flight.aircraft.aircraftName,
+    	            "€" + ticket.price
+    	        ])),
+    	    },
+    	    footer: {text: "The invoice is created on a computer and is valid without the signature and stamp.",},
+    	    pageEnable: true,
+    	    pageLabel: "Page "
+	};  
+	
+	var pdfObject = jsPDFInvoiceTemplate.default(props);
+}
+
 
 function click(button, scope, http){
     switch (button) {
@@ -27,7 +92,7 @@ app.controller('listTicketsCtrl', function($scope,$http, $q, $timeout, $window) 
     initVars($scope, $window);
     initView($scope);
     $scope.doClick=function(button) {click(button, $scope,$http);}
-    console.log($scope.user)
+    $scope.printPdf=function(ticket) {printPdf(ticket);}
     var request = $http.get("../rest/searchticket", {params: $scope.user}).then(function(response) {
         if (response.status == 200 || response.status == 204) {
         	console.log("Success on searching tickets."); 
